fda6f99e00b0d0c133f1d12699472ff9f13a83bc
snowmeow <yuki754685421@163.com>
2024-08-01 13:49:27 +0800

    feat(leetcode): GroupAnagrams

--- src/main/java/leetcode/GroupAnagrams.java
+++ src/main/java/leetcode/GroupAnagrams.java
@@ -1,8 +1,22 @@
 package leetcode;
 
+import java.util.*;
+
 /**
  * <a href="https://leetcode.cn/problems/group-anagrams">problem</a>
  * @author snowmeow
  */
 public class GroupAnagrams {
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String sortStr = new String(chars);
+            map.computeIfAbsent(sortStr, k -> new ArrayList<>()).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
 }
